2cb64087ca70d48231adcf8d9c745e7a65cf086a
Encina Agustin <agustin72encina@gmail.com>
2025-01-26 13:31:34 -0300

    nuevos enlaces

--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_BACKEND= 'http://localhost:8080';
-export const URL_FRONTEND= 'opticaurbana.com';
+export const URL_BACKEND= 'https://opticaurbana.com.ar:8080/api';
+export const URL_FRONTEND= 'https://opticaurbana.com.ar';
