47eb86d2dcbe7bfcf3033903e690304388beacfd
Encina Agustin <agustin72encina@gmail.com>
2025-01-26 15:45:50 -0300

    cambio en url angular

--- frontend/src/app/config/config.ts
+++ frontend/src/app/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_BACKEND= 'https://opticaurbana.com.ar:8080/api';
+export const URL_BACKEND= 'https://opticaurbana.com.ar/api';
 export const URL_FRONTEND= 'https://opticaurbana.com.ar';
